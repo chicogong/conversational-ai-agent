a3eb735d94c8efe9c9fc4a744f78d28cdf2617d3
chico <chicogong@tencent.com>
2025-05-26 10:11:26 +0800

    daji in fist agent card

--- src/agent_cards/index.js
+++ src/agent_cards/index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
+    daji: require('./daji'),
     default: require('./default'),
     sweet_girl: require('./sweet_girl'),
-    daji: require('./daji'),
     group_chat: require('./group_chat'),
     mcp_tencent_map: require('./mcp_tencent_map')
 }; 
